74447d106c905aeaa2fcc08745b82208e41c18e1
Tuny <tongzhu.liu@gmail.com>
2016-03-30 23:58:01 +0800

    修改自动化安装jetty脚本

--- server_env_config/jdk_jetty_mysql/jetty_9.2.15.v20160210.sh
+++ server_env_config/jdk_jetty_mysql/jetty_9.2.15.v20160210.sh
@@ -4,13 +4,19 @@ then
     echo "Please run with super user"
     exit 1
 fi
-
-JETTY_FILE="jetty-distribution-9.2.15.v20160210.tar.gz"
+DOWNLOAD_DIR="./downloads"
+JETTY_FILE="$DOWNLOAD_DIR/jetty-distribution-9.2.15.v20160210.tar.gz"
 JETTY_DURL="http://download.eclipse.org/jetty/9.2.15.v20160210/dist/jetty-distribution-9.2.15.v20160210.tar.gz"
 JETTY_INSTALL_DIR="/usr/local/jetty"
-JETTY_ROOT_FOLDER="jetty_9.2.15"
-JETTY_STD_FOLDER="/usr/jetty"
+JETTY_ROOT_FOLDER="jetty-distribution-9.2.15.v20160210"
+SERVER_STD_FOLDER="/usr/server"
 PROFILE="/etc/profile"
+
+if [ ! -d "$DOWNLOAD_DIR" ];
+then
+    echo "ehll"
+    mkdir -p "$DOWNLOAD_DIR"
+fi
 
 if [ ! -f $JETTY_FILE ]; then
     wget --no-check-certificate -c --header "Cookie: oraclelicense=accept-securebackup-cookie" $JETTY_DURL -O ./$JETTY_FILE
@@ -38,18 +44,18 @@ fi
 
 tar -zxvf $JETTY_FILE -C $JETTY_INSTALL_DIR
 
-if [ ! -d "$JETTY_STD_FOLDER" ];
+if [ ! -d "$SERVER_STD_FOLDER" ];
 then
-    mkdir -p "$JETTY_STD_FOLDER"
+    mkdir -p "$SERVER_STD_FOLDER"
 fi
 
-if [ ! -L"$JETTY_STD_FOLDER/9.2.15" ];
+if [ ! -L $SERVER_STD_FOLDER/$JETTY_ROOT_FOLDER ];
 then
-    ln -s "$JETTY_INSTALL_DIR/$JETTY_ROOT_FOLDER" "$JETTY_STD_FOLDER/9.2.15"
+    ln -s $JETTY_INSTALL_DIR/$JETTY_ROOT_FOLDER $SERVER_STD_FOLDER/$JETTY_ROOT_FOLDER
 fi
 
-echo "JETTY_HOME=$JETTY_STD_FOLDER/9.2.15" >> "$PROFILE" 
-echo "PATH=$JETTY_STD_FOLDER/9.2.15/bin:$PATH" >> "$PROFILE"
+echo "JETTY_HOME=$SERVER_STD_FOLDER/$JETTY_ROOT_FOLDER" >> "$PROFILE" 
+echo "PATH=$SERVER_STD_FOLDER/$JETTY_ROOT_FOLDER/bin:$PATH" >> "$PROFILE"
 source "$PROFILE"
 
 
